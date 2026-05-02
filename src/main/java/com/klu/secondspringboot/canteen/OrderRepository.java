2c063c59947524434d6a12f52461394d2285052f
Reddy1236 <2400033275@kluniversity.in>
2026-05-02 19:08:45 +0530

    Remove all project files

--- src/main/java/com/klu/secondspringboot/canteen/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.klu.secondspringboot.canteen;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
